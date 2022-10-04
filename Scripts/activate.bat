c045a49f57636661f59b99a89712870dbed39c2e
Samu-Dev <samcorredor1@gmail.com>
2022-10-03 20:34:49 -0500

    cambio ubicacion front

--- Scripts/activate.bat
+++ Scripts/activate.bat
@@ -8,7 +8,7 @@ if defined _OLD_CODEPAGE (
     "%SystemRoot%\System32\chcp.com" 65001 > nul
 )
 
-set VIRTUAL_ENV=C:\Users\SAMUEL CORREDOR\Documents\2022MISIONTIC\CICLO3\PROGRAMACION\proyecto_ciclo3
+set VIRTUAL_ENV=C:\Users\SAMUEL CORREDOR\documents\2022MISIONTIC\CICLO3\PROGRAMACION\HomeElectric
 
 if not defined PROMPT set PROMPT=$P$G
 
@@ -16,7 +16,7 @@ if defined _OLD_VIRTUAL_PROMPT set PROMPT=%_OLD_VIRTUAL_PROMPT%
 if defined _OLD_VIRTUAL_PYTHONHOME set PYTHONHOME=%_OLD_VIRTUAL_PYTHONHOME%
 
 set _OLD_VIRTUAL_PROMPT=%PROMPT%
-set PROMPT=(proyecto_ciclo3) %PROMPT%
+set PROMPT=(HomeElectric) %PROMPT%
 
 if defined PYTHONHOME set _OLD_VIRTUAL_PYTHONHOME=%PYTHONHOME%
 set PYTHONHOME=
@@ -25,7 +25,7 @@ if defined _OLD_VIRTUAL_PATH set PATH=%_OLD_VIRTUAL_PATH%
 if not defined _OLD_VIRTUAL_PATH set _OLD_VIRTUAL_PATH=%PATH%
 
 set PATH=%VIRTUAL_ENV%\Scripts;%PATH%
-set VIRTUAL_ENV_PROMPT=(proyecto_ciclo3) 
+set VIRTUAL_ENV_PROMPT=(HomeElectric) 
 
 :END
 if defined _OLD_CODEPAGE (
